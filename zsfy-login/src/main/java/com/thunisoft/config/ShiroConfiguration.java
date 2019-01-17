e729c18a9cfcb5e4a477bac0b4c676094ec7fe8c
张鹏举 <zhangpengju@thunisoft.com>
2019-01-17 13:27:37 +0800

    更新对应状态

---
+++ zsfy-login/src/main/java/com/thunisoft/config/ShiroConfiguration.java
@@ -0,0 +1,69 @@
+package com.thunisoft.config;
+
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author ZhPJ
+ * @Date 2019/1/15 001510:10
+ * @Version 1.0
+ * @Description:
+ */
+@Configuration
+public class ShiroConfiguration extends DefaultWebSessionManager {
+
+    //将自己的验证方式加入容器
+    @Bean
+    public ZsfyShiroRealm myShiroRealm() {
+        ZsfyShiroRealm zsfyShiroRealm = new ZsfyShiroRealm();
+        return zsfyShiroRealm;
+    }
+
+    @Bean(value = "securityManger")
+    public DefaultWebSecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(myShiroRealm());
+        return securityManager;
+    }
+
+    @Bean
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        Map<String,String> chainsMap = new HashMap<String, String>();
+        //登出
+//        chainsMap.put("/logout","logout");
+        //登录
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        //首页
+//        shiroFilterFactoryBean.setSuccessUrl("/index");
+        //错误页面，认证不通过跳转
+//        shiroFilterFactoryBean.setUnauthorizedUrl("/error");
+        //对所有用户认证
+        chainsMap.put("/**","authc");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(chainsMap);
+        return shiroFilterFactoryBean;
+    }
+
+    //加入注解的使用，不加入这个注解不生效
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
+    }
+}
---
+++ zsfy-login/src/main/java/com/thunisoft/config/ZsfyShiroRealm.java
@@ -0,0 +1,59 @@
+package com.thunisoft.config;
+
+import com.thunisoft.bean.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author ZhPJ
+ * @Date 2019/1/15 001512:04
+ * @Version 1.0
+ * @Description:
+ */
+public class ZsfyShiroRealm extends AuthorizingRealm {
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        String username = (String) SecurityUtils.getSubject().getPrincipal();
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        //获得该用户角色
+        String role = "111";
+        Set<String> set = new HashSet<>();
+        //需要将 role 封装到 Set 作为 info.setRoles() 的参数
+        set.add(role);
+        //设置该用户拥有的角色
+        info.setRoles(set);
+        return info;
+    }
+
+    /**
+     * @deprecation: 用户认证
+     * @param:
+     * @return:
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+        //加这一步的目的是在Post请求的时候会先进认证，然后在到请求
+        if (token.getPrincipal() == null) {
+            return null;
+        }
+        //获取用户信息
+        User user = new User();//userService.login(token.getUsername());
+        if (user == null) {
+            //这里返回后会报出对应异常
+            return null;
+        } else {
+            //这里验证authenticationToken和simpleAuthenticationInfo的信息
+            SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(token.getPrincipal(), user.getPassword(), getName());
+            return simpleAuthenticationInfo;
+        }
+    }
+}
